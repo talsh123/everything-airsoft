a4b5c2dff842faf722ba4e3903afeaf1e43da1d1
Talsh <talshalom900@gmail.com>
2021-09-29 19:59:23 +0300

    Manufacturer schema
    This simple commit adds the schema containing all the manufacturers that
    are represented in the Everything Airsoft website.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
+
 function App() {
-  return <div className="App"></div>;
+  return <React.Fragment></React.Fragment>;
 }
 
 export default App;
---
+++ schemas/Manufacturer.js
@@ -0,0 +1,24 @@
+// Import statements
+const mongoose = require('mongoose');
+
+// Define schema
+const schema = mongoose.Schema;
+
+const manufacturerSchema = new schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true,
+  },
+  info: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true,
+  },
+});
+
+const manufacturer = mongoose.model('Manufacturer', manufacturerSchema);
+
+module.exports = manufacturer;
